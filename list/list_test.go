6b945697abe76d88f3f53a827eab84c688a79f42
jdlau <jdlau@qq.com>
2020-03-22 15:42:39 +0800

    list/: 添加生成代码测试

--- list/list_test.go
+++ list/list_test.go
@@ -1,8 +1,10 @@
 package list
 
 import (
+	"reflect"
 	"testing"
 
+	"github.com/donnol/gen/list/testdata1"
 	"github.com/donnol/gen/parser"
 	"github.com/donnol/gen/template"
 )
@@ -21,5 +23,40 @@ func TestList(t *testing.T) {
 		if err != nil {
 			t.Fatalf("%+v\n", err)
 		}
+	}
+}
+
+func TestListGenResult(t *testing.T) {
+	ml := testdata1.ModelList{
+		{ID: 1, Name: "jd", Age: 31.0, UserID: 1, ContentID: 1, ModelID: 1, AddrID: 1},
+	}
+
+	cr := ml.ColumnID()
+	cwant := []int{1}
+	if !reflect.DeepEqual(cr, cwant) {
+		t.Fatalf("Bad result: %+v != %+v\n", cr, cwant)
+	}
+
+	mr := ml.MapName()
+	mwant := map[string]testdata1.Model{"jd": ml[0]}
+	if !reflect.DeepEqual(mr, mwant) {
+		t.Fatalf("Bad result: %+v != %+v\n", mr, mwant)
+	}
+
+	mlr := ml.MapListByName()
+	mlwant := map[string][]testdata1.Model{"jd": ml}
+	if !reflect.DeepEqual(mr, mwant) {
+		t.Fatalf("Bad result: %+v != %+v\n", mlr, mlwant)
+	}
+
+	jur := ml.JoinUserByUserIDEqualID([]testdata1.User{{ID: 1, Name: "jd"}}, func(m testdata1.Model, u testdata1.User) testdata1.Model {
+		m.UserName = u.Name
+		return m
+	})
+	jurwant := testdata1.ModelList{
+		{ID: 1, Name: "jd", Age: 31.0, UserID: 1, UserName: "jd", ContentID: 1, ModelID: 1, AddrID: 1},
+	}
+	if !reflect.DeepEqual(mr, mwant) {
+		t.Fatalf("Bad result: %+v != %+v\n", jur, jurwant)
 	}
 }
